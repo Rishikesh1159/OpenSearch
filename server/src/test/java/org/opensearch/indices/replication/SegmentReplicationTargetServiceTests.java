ac0608d2e1b05874d4d61738417aee46e780ab27
Poojita Raj <poojiraj@amazon.com>
2022-06-21 18:13:27 -0700

    Cherry-picking https://github.com/opensearch-project/OpenSearch/pull/3533.
    Signed-off-by: Rishikesh1159 <rishireddy1159@gmail.com>

--- server/src/test/java/org/opensearch/indices/replication/SegmentReplicationTargetServiceTests.java
+++ server/src/test/java/org/opensearch/indices/replication/SegmentReplicationTargetServiceTests.java
@@ -28,6 +28,7 @@ import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 import static org.mockito.Mockito.times;
 
+
 public class SegmentReplicationTargetServiceTests extends IndexShardTestCase {
 
     private IndexShard indexShard;
@@ -76,6 +77,8 @@ public class SegmentReplicationTargetServiceTests extends IndexShardTestCase {
         );
         final SegmentReplicationTarget spy = Mockito.spy(target);
         doAnswer(invocation -> {
+            // setting stage to REPLICATING so transition in markAsDone succeeds on listener completion
+            target.state().setStage(SegmentReplicationState.Stage.REPLICATING);
             final ActionListener<Void> listener = invocation.getArgument(0);
             listener.onResponse(null);
             return null;
@@ -98,7 +101,7 @@ public class SegmentReplicationTargetServiceTests extends IndexShardTestCase {
 
                 @Override
                 public void onReplicationFailure(SegmentReplicationState state, OpenSearchException e, boolean sendShardFailure) {
-                    assertEquals(SegmentReplicationState.Stage.INIT, state.getStage());
+                    assertEquals(SegmentReplicationState.Stage.REPLICATING, state.getStage());
                     assertEquals(expectedError, e.getCause());
                     assertTrue(sendShardFailure);
                 }
@@ -106,6 +109,8 @@ public class SegmentReplicationTargetServiceTests extends IndexShardTestCase {
         );
         final SegmentReplicationTarget spy = Mockito.spy(target);
         doAnswer(invocation -> {
+            // setting stage to REPLICATING so transition in markAsDone succeeds on listener completion
+            target.state().setStage(SegmentReplicationState.Stage.REPLICATING);
             final ActionListener<Void> listener = invocation.getArgument(0);
             listener.onFailure(expectedError);
             return null;
